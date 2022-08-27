6440abcaae5c755c79e4b65f0c331b7998b621f8
Veysel <veyselanik@sabanciuniv.edu>
2022-08-27 19:27:53 +0300

    new changes

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const http = require('http');
+const { consumers } = require('stream');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, {
-        'Content-Type': 'text/plain',  
-    });
-    res.end('Server init');
+const server = http.createServer();
+
+const friends = [
+    {
+        id: 0,
+        name: 'Fatih Terim',
+    },
+    {
+        id: 1,
+        name: 'Muslera',
+    },
+    {
+        id: 2,
+        name: 'Drogba',
+    }
+]
+
+server.on('request', (req, res) =>{
+    const items = req.url.split('/');
+    if (items[1] === 'friends'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3){
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        }
+        else{
+            res.end(JSON.stringify(friends));
+        }
+        
+    }
+    else if (items[1] === 'messages'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Sampiyon Cimbom!</li>');
+        res.write('<li>Senior Terim!</li>');
+        res.write('<ul>');
+        res.write('<body>');
+        res.write('<html>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 server.listen(PORT, ()=>{
